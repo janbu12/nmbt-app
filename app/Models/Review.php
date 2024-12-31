27d7873bcb53a7ad060a870f2ad93961522e882e
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2025-01-01 03:36:16 +0700

    make migration rent, rent_details, reviews

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Review extends Model
 {
     protected $fillable = [
-        'product_id', 'user_id', 'rating', 'review_text',
+        'product_id', 'user_id', 'rating', 'content',
     ];
 
     public function product(){
