dc84e2574f55ec0cfbbed809fb836ee8dc891e04
Muhammad Mizan Al Mujadid <mizanmujadid2436@gmail.com>
2025-01-01 05:05:31 +0700

    edit review factory and make review seeder

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -13,10 +13,10 @@ class Review extends Model
     ];
 
     public function product(){
-        return $this->belongsTo(ProductRentModel::class);
+        return $this->belongsTo(ProductRentModel::class, 'product_id');
     }
 
     public function user(){
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, 'user_id');
     }
 }
--- database/factories/ReviewFactory.php
+++ database/factories/ReviewFactory.php
@@ -2,36 +2,17 @@
 
 namespace Database\Factories;
 
-use App\Models\ProductRentModel as Product;
-use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
- */
 class ReviewFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     protected $model = \App\Models\Review::class;
-
-    /**
-     * Configure the model's factory.
-     *
-     * @return void
-     */
 
     public function definition(): array
     {
-        $user = User::inRandomOrder()->first();
-        $product = Product::inRandomOrder()->first();
-
         return [
-            'product_id' => $product->id,
-            'user_id' => $user->id,
+            'product_id' => null, // Akan diisi oleh Seeder
+            'user_id' => null,    // Akan diisi oleh Seeder
             'rating' => $this->faker->numberBetween(3, 5),
             'comment' => $this->faker->text(100),
         ];
