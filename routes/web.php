b6375da5507f470e6b64e56d398e7f2a69c0898e
rapilap <raffyabdillah03@gmail.com>
2025-01-01 22:41:23 +0700

    invoice frontend

--- routes/web.php
+++ routes/web.php
@@ -66,6 +66,6 @@ Route::post('/email/verification-notification', function (Request $request) {
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
-Route::get('coba', function() {
-    return view('coba');
+Route::get('/invoice', function() {
+    return view('invoice');
 });
