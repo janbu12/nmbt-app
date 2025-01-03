5b60303a669f790bed40e2b079d41cdc9f5146ea
janbu12 <mizanmujadid2436@gmail.com>
2025-01-04 04:09:37 +0700

    refactor: attribute table cart

--- database/factories/CartFactory.php
+++ database/factories/CartFactory.php
@@ -28,9 +28,6 @@ class CartFactory extends Factory
         return [
             'user_id' => $user->id,
             'product_id' => $product->id,
-            'name' => $this->faker->words(3, true), // Nama produk
-            'image_url' => $this->faker->imageUrl(200, 200, 'hiking equipment'), // URL gambar
-            'price' => $this->faker->randomFloat(2, 100000, 500000), // Harga produk (100k - 500k)
             'quantity' => $this->faker->numberBetween(1, 10), // Jumlah produk
         ];
     }
--- database/migrations/2024_12_26_133658_create_carts_table.php
+++ database/migrations/2024_12_26_133658_create_carts_table.php
@@ -14,10 +14,7 @@ return new class extends Migration
         Schema::create('carts', function (Blueprint $table) {
             $table->id(); // Kolom primary key
             $table->unsignedBigInteger('user_id'); // Foreign key ke tabel users
-            $table->unsignedBiginteger('product_id');
-            $table->string('name'); // Nama produk
-            $table->string('image_url')->nullable(); // URL gambar produk
-            $table->decimal('price', 10, 2); // Harga produk
+            $table->unsignedBiginteger('product_id'); // Foreign key ke tabel products
             $table->integer('quantity')->default(1); // Jumlah produk
             $table->timestamps(); // Kolom created_at dan updated_at
 
