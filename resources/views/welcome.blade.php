95de5c36809db827614680741576830b3b3c11e5
janbu12 <mizanmujadid2436@gmail.com>
2025-01-04 22:11:00 +0700

    update: carousel section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,14 @@
             <h1 class="text-bg3 text-3xl font-medium text-center">Nordic Mountain Bound Travelers</h1>
         </div>
     </div>
-    <div class="py-16">
-        ashj
+    <div class="px-12 flex flex-col text-tertiery1">
+
+        {{-- Carousel Section --}}
+        <div class="py-16 flex flex-col items-center">
+            <p class="text-center text-2xl lg:max-w-[500px]">
+                Menyediakan semua kebutuhan camping yang anda butuhkan kapanpun yang anda inginkan
+            </p>
+        </div>
+
     </div>
 </x-app-layout>
