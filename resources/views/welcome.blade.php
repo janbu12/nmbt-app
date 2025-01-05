b8aaff673c509d5f2aed59e8be6c167e5f6790fe
janbu12 <mizanmujadid2436@gmail.com>
2025-01-06 05:07:15 +0700

    refactor: behavior navbar on landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,32 +22,32 @@
                     </div>
                     <!-- Slide 2 -->
                     <div class="flex-shrink-0 w-full grid grid-cols-4">
-                        <img src="{{ asset('images/sliders/camp5.png') }}" alt="Camp 5" class="w-full h-auto shadow-lg" />
-                        <img src="{{ asset('images/sliders/camp6.png') }}" alt="Camp 6" class="w-full h-auto shadow-lg" />
-                        <img src="{{ asset('images/sliders/camp7.png') }}" alt="Camp 7" class="w-full h-auto shadow-lg" />
-                        <img src="{{ asset('images/sliders/camp8.png') }}" alt="Camp 8" class="w-full h-auto shadow-lg" />
+                        <img src="{{ asset('images/sliders/camp1.png') }}" alt="Camp 5" class="w-full h-auto shadow-lg" />
+                        <img src="{{ asset('images/sliders/camp2.png') }}" alt="Camp 6" class="w-full h-auto shadow-lg" />
+                        <img src="{{ asset('images/sliders/camp3.png') }}" alt="Camp 7" class="w-full h-auto shadow-lg" />
+                        <img src="{{ asset('images/sliders/camp4.png') }}" alt="Camp 8" class="w-full h-auto shadow-lg" />
                     </div>
                     <div class="flex-shrink-0 w-full grid grid-cols-4">
-                        <img src="{{ asset('images/sliders/camp9.png') }}" alt="Camp 9" class="w-full h-auto shadow-lg" />
-                        <img src="{{ asset('images/sliders/camp10.png') }}" alt="Camp 10" class="w-full h-auto shadow-lg" />
-                        <img src="{{ asset('images/sliders/camp11.png') }}" alt="Camp 11" class="w-full h-auto shadow-lg" />
-                        <img src="{{ asset('images/sliders/camp12.png') }}" alt="Camp 12" class="w-full h-auto shadow-lg" />
+                        <img src="{{ asset('images/sliders/camp1.png') }}" alt="Camp 9" class="w-full h-auto shadow-lg" />
+                        <img src="{{ asset('images/sliders/camp2.png') }}" alt="Camp 10" class="w-full h-auto shadow-lg" />
+                        <img src="{{ asset('images/sliders/camp3.png') }}" alt="Camp 11" class="w-full h-auto shadow-lg" />
+                        <img src="{{ asset('images/sliders/camp4.png') }}" alt="Camp 12" class="w-full h-auto shadow-lg" />
                     </div>
                 </div>
 
                 <!-- Navigation Buttons -->
-                <button id="prevBtn" class="absolute top-1/2 left-4 transform -translate-y-1/2 bg-gray-700 text-white p-2 rounded-full shadow-lg">
+                <button id="prevBtn" class="absolute top-1/2 left-4 transform -translate-y-1/2 bg-gray-700 hover:bg-gray-600 text-white p-2 rounded shadow-lg">
                     &#10094;
                 </button>
-                <button id="nextBtn" class="absolute top-1/2 right-4 transform -translate-y-1/2 bg-gray-700 text-white p-2 rounded-full shadow-lg">
+                <button id="nextBtn" class="absolute top-1/2 right-4 transform -translate-y-1/2 bg-gray-700 hover:bg-gray-600 text-white p-2 rounded shadow-lg">
                     &#10095;
                 </button>
 
                 <!-- Pagination Dots -->
                 <div class="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
-                    <button class="dot w-3 h-3 bg-gray-300 rounded-full"></button>
-                    <button class="dot w-3 h-3 bg-gray-300 rounded-full"></button>
-                    <button class="dot w-3 h-3 bg-gray-300 rounded-full"></button>
+                    <button class="dot w-3 h-3 bg-gray-100 border-none hover:bg-gray-400 rounded-full"></button>
+                    <button class="dot w-3 h-3 bg-gray-100 border-none hover:bg-gray-400 rounded-full"></button>
+                    <button class="dot w-3 h-3 bg-gray-100 border-none hover:bg-gray-400 rounded-full"></button>
                 </div>
             </div>
             <p class="text-center text-2xl lg:max-w-[500px] mt-20">
@@ -72,8 +72,13 @@
 
                     // Update active dot
                     dots.forEach((dot, index) => {
-                        dot.classList.toggle('bg-gray-700', index === currentIndex);
-                        dot.classList.toggle('bg-gray-300', index !== currentIndex);
+                        if (index === currentIndex) {
+                            dot.classList.add('bg-gray-700', 'border', 'hover:bg-gray-600', 'border-secondary1');
+                            dot.classList.remove('bg-gray-100', 'border-none', 'hover:bg-gray-400');
+                        } else {
+                            dot.classList.add('bg-gray-100', 'border-none', 'hover:bg-gray-400');
+                            dot.classList.remove('bg-gray-700', 'border', 'hover:bg-gray-600', 'border-secondary1');
+                        }
                     });
                 }
 
@@ -105,14 +110,13 @@
 
             document.addEventListener('DOMContentLoaded', () => {
                 const navbar = document.getElementById('navbar');
-                const navbarLinks = document.querySelectorAll('#navbar-links a');
+                const navbarLinks = document.querySelectorAll('.nav-link');
+                const loginBtn = document.getElementById('login-btn');
                 const mainBackground = document.getElementById('main-background');
                 const transparentClasses = 'bg-transparent text-white';
                 const blackClasses = 'bg-white text-tertiery1';
                 const transparentLinkClasses = 'text-white hover:text-gray-300';
                 const blackLinkClasses = 'text-tertiery1 hover:text-tertiery1';
-
-                console.log(navbarLinks);
 
                 function handleScroll() {
                     const mainBackgroundHeight = mainBackground.offsetHeight;
@@ -123,19 +127,30 @@
                         navbar.classList.remove(...transparentClasses.split(' '));
                         navbar.classList.add(...blackClasses.split(' '));
 
+                        // Ubah navbar link
                         navbarLinks.forEach(link => {
                             link.classList.remove(...transparentLinkClasses.split(' '));
                             link.classList.add(...blackLinkClasses.split(' '));
                         });
+
+                        // Ubah login button
+                        loginBtn.classList.remove('btn-tertiery-custom');
+                        loginBtn.classList.add('btn-secondary-custom');
                     } else {
                         // Reset navbar ke transparent
                         navbar.classList.remove(...blackClasses.split(' '));
                         navbar.classList.add(...transparentClasses.split(' '));
 
+                        // Ubah login button
                         navbarLinks.forEach(link => {
                             link.classList.remove(...blackLinkClasses.split(' '));
                             link.classList.add(...transparentLinkClasses.split(' '));
                         });
+
+                        // Ubah login button
+
+                        loginBtn.classList.remove('btn-secondary-custom');
+                        loginBtn.classList.add('btn-tertiery-custom');
                     }
                 }
 
